e5c4e0e20aa68947947f89fe31799b3de544404b
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2023-06-02 12:48:56 -0300

    Added Font Awesome

--- src/main.js
+++ src/main.js
@@ -2,5 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-createApp(App).use(store).use(router).mount('#app')
+// test
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+// add icons to the library
+library.add(faUserSecret)
+
+
+createApp(App).use(store).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="id-home">
     <h1>This is an home page</h1>
+
+    <font-awesome-icon :icon="['fas', 'user-secret']" />
   </div>
 </template>
 
