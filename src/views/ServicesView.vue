bbad24453ffa7f3b01729e30d62837666852c423
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2023-06-07 13:57:53 -0300

    Added services/contact component

--- src/views/ServicesView.vue
+++ src/views/ServicesView.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="id-services">
     <Banner />
-    <WeWork />
+    <div class="container">
+      <WeWork />
+      <Contact />
+    </div>
   </div>
 </template>
 
 <script>
 import Banner from '@/components/services/banner.vue'
 import WeWork from '@/components/services/weWork.vue'
+import Contact from '@/components/contact.vue'
+
 export default {
   name: 'ServicesView',
   components: {
-    Banner, WeWork
+    Banner, WeWork, Contact
   }
 }
 </script>
+
