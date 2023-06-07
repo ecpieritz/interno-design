c3958bfa9d8ddeb551066b3f2bae0b6eb17beb23
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2023-06-07 12:43:55 -0300

    Changed home/contact class

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -1,6 +1,6 @@
 
 <template>
-  <div class="id-contact container" id="id-contact">
+  <div class="id-contact-cp container" id="id-contact-cp">
     <h2>Wanna join the interno?</h2>
     <p>It is a long established fact  will be distracted.</p>
     <router-link to="/contact" class="btn gold-btn">Contact With Us <font-awesome-icon :icon="['fas', 'arrow-right']" /></router-link>
@@ -18,8 +18,8 @@ export default {
 <style lang="scss">
 @import '@/styles.scss';
 
-.id-contact {
-    margin: 160px 0 80px ;
+.id-contact-cp {
+    margin: 160px 0 80px !important;
     padding: 80px;
     background-color: $id-gray;
     border-radius: 50px;
